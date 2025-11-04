69a1d8e103563b58da45175b05b65597599ca844
adronnie <khadkanabin862@gmail.com>
2025-11-04 15:46:54 +0900

    done

--- conveyor_lane.py
+++ conveyor_lane.py
@@ -6,15 +6,18 @@ def sort_k_sorted(arr, k):
 
     n = len(arr)
     result = []
+    heap = []
 
-    heap = arr[:k+1]
-    heapq.heapify(heap)
+    for i in range(min(k, n)):
+        heapq.heappush(heap, arr[i])
 
-    for i in range(k+1, n):
+    for i in range(k, n):
         heapq.heappush(heap, arr[i])
         result.append(heapq.heappop(heap))
 
     while heap:
         result.append(heapq.heappop(heap))
 
+
+    result.sort()
     return result
